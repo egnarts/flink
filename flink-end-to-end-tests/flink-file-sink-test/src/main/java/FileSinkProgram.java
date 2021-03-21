f17375e92c8ca043280a63e3abbfd4686808c806
EC2 Default User <ec2-user@ip-172-31-36-202.ap-northeast-2.compute.internal>
2021-03-21 12:49:40 +0000

    spot

--- flink-end-to-end-tests/flink-file-sink-test/src/main/java/FileSinkProgram.java
+++ flink-end-to-end-tests/flink-file-sink-test/src/main/java/FileSinkProgram.java
@@ -58,12 +58,13 @@ public enum FileSinkProgram {
     ;
 
     public static class DefaultRowFormatBuilder
-            extends StreamingFileSink.RowFormatBuilder<Tuple2<Integer, Integer>, String,
-            DefaultRowFormatBuilder> {
+            extends StreamingFileSink.RowFormatBuilder<
+                    Tuple2<Integer, Integer>, String, DefaultRowFormatBuilder> {
         private static final long serialVersionUID = -8503344257202146718L;
 
         private DefaultRowFormatBuilder(
-                Path basePath, Encoder<Tuple2<Integer, Integer>> encoder,
+                Path basePath,
+                Encoder<Tuple2<Integer, Integer>> encoder,
                 BucketAssigner<Tuple2<Integer, Integer>, String> bucketAssigner) {
             super(basePath, encoder, bucketAssigner);
         }
@@ -86,17 +87,19 @@ public enum FileSinkProgram {
         DataStream<Tuple2<Integer, Integer>> source = env.addSource(new Generator(10, 10, 60));
 
         if (sinkToTest.equalsIgnoreCase("StreamingFileSink")) {
-            DefaultRowFormatBuilder builder = new DefaultRowFormatBuilder(
-                    new Path(
-                            outputPath),
-                    (Encoder<Tuple2<Integer, Integer>>)
-                            (element, stream) -> {
-                                PrintStream out = new PrintStream(stream);
-                                out.println(element.f1);
-                            },
-                    new KeyBucketAssigner()).withRollingPolicy(OnCheckpointRollingPolicy.build());
+            DefaultRowFormatBuilder builder =
+                    new DefaultRowFormatBuilder(
+                                    new Path(outputPath),
+                                    (Encoder<Tuple2<Integer, Integer>>)
+                                            (element, stream) -> {
+                                                PrintStream out = new PrintStream(stream);
+                                                out.println(element.f1);
+                                            },
+                                    new KeyBucketAssigner())
+                            .withRollingPolicy(OnCheckpointRollingPolicy.build());
 
-            StreamingFileSink sink = new StreamingFileSink(builder, DEFAULT_BUCKET_CHECK_INTERVAL) {};
+            StreamingFileSink sink =
+                    new StreamingFileSink(builder, DEFAULT_BUCKET_CHECK_INTERVAL) {};
             source.keyBy(0).addSink(sink);
         } else if (sinkToTest.equalsIgnoreCase("FileSink")) {
             FileSink<Tuple2<Integer, Integer>> sink =
