b69534e2ee8f7fb1b994abd1163b42592f5d04ad
Jeongho Jang <jeongho.jang@sk.com>
2021-03-24 18:59:28 +0900

    implement state api

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/state/StateApi.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/state/StateApi.java
@@ -6,7 +6,6 @@ import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.common.typeutils.base.IntSerializer;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.MultipleParameterTool;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.runtime.state.FunctionInitializationContext;
