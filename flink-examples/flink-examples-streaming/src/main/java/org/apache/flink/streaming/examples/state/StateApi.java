119ab41fccd738a716ad9b12304218bd8a4ed746
Jeongho Jang <jeongho.jang@sk.com>
2021-03-24 18:58:45 +0900

    implement state api

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/state/StateApi.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/state/StateApi.java
@@ -5,15 +5,87 @@ import org.apache.flink.api.common.state.ListStateDescriptor;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.common.typeutils.base.IntSerializer;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.MultipleParameterTool;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.util.Collector;
 
 /** */
 public class StateApi {
+    /** */
+    public static final class Generator
+            implements SourceFunction<Integer>, CheckpointedFunction {
+
+        private static final long serialVersionUID = -2819385275681175792L;
+
+        private final int numKeys;
+        private final int idlenessMs;
+        private final int recordsToEmit;
+
+        private volatile int numRecordsEmitted = 0;
+        private volatile boolean canceled = false;
+
+        private ListState<Integer> state = null;
+
+        Generator(final int numKeys, final int idlenessMs, final int durationSeconds) {
+            this.numKeys = numKeys;
+            this.idlenessMs = idlenessMs;
+
+            this.recordsToEmit = ((durationSeconds * 1000) / idlenessMs) * numKeys;
+        }
+
+        @Override
+        public void run(final SourceContext<Integer> ctx) throws Exception {
+            while (numRecordsEmitted < recordsToEmit) {
+                synchronized (ctx.getCheckpointLock()) {
+                    for (int i = 0; i < numKeys; i++) {
+                        ctx.collect(i);
+                        numRecordsEmitted++;
+                    }
+                }
+                Thread.sleep(idlenessMs);
+            }
+
+            while (!canceled) {
+                Thread.sleep(50);
+            }
+        }
+
+        @Override
+        public void cancel() {
+            canceled = true;
+        }
+
+        @Override
+        public void initializeState(FunctionInitializationContext context) throws Exception {
+            state =
+                    context.getOperatorStateStore()
+                            .getListState(
+                                    new ListStateDescriptor<Integer>(
+                                            "state", IntSerializer.INSTANCE));
+
+            for (Integer i : state.get()) {
+                numRecordsEmitted += i;
+            }
+        }
+
+        @Override
+        public void snapshotState(FunctionSnapshotContext context) throws Exception {
+            state.clear();
+            state.add(numRecordsEmitted);
+        }
+    }
+
     /** */
     public static class StatefulFunctionWithTime
             extends KeyedProcessFunction<Integer, Integer, Void> {
@@ -50,21 +122,26 @@ public class StateApi {
 
         // make parameters available in the web interface
         env.getConfig().setGlobalJobParameters(params);
+        env.enableCheckpointing(5000);
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setCheckpointTimeout(60000);
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
 
         // get input data
-        DataStream<Integer> text = env.fromElements(1, 2, 3);
+        DataStream<Integer> source = env.addSource(new Generator(10, 10, 60));
+//        DataStream<Integer> text = env.fromElements(1, 2, 3);
 
         DataStream<Void> counts =
-                text.keyBy(value -> value).process(new StatefulFunctionWithTime());
+                source.keyBy(value -> value).process(new StatefulFunctionWithTime());
 
         // emit result
         if (params.has("output")) {
             counts.writeAsText(params.get("output"));
         } else {
-            System.out.println("Printing result to stdout. Use --output to specify output path.");
             counts.print();
         }
         // execute program
-        env.execute("Streaming WordCount");
+        env.execute("Streaming StateApi");
     }
 }
